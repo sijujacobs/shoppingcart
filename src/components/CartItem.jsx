4cd25b9c2ec39f249f0ec06bf3cabcb36ed49060
SIJU JACOB <SIJUJACOBS@GMAILCOM>
2020-01-25 22:11:24 -0500

    added gh-pages and CSS changes

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,31 +1,30 @@
 import React from 'react';
 
-import {connect} from 'react-redux';
-import {removeItem} from '../redux/actions/index';
-// import {CSSTransition} from 'react-transition-group';
+import { connect } from 'react-redux';
+import { removeItem } from '../redux/actions/index';
 
 const CartItem = (props) => {
     const removeItemHandler = (selectedProduct) => {
         props.removeItem(selectedProduct);
     }
-    return(<div className="cartItem">
-         <div className="itemImage">
-            <img alt="itemImage" src={props.item.thumbnailUrl}/>
-        </div>
-        <div className="itemDesc">
-                {props.item.title.substring(0, 10)}
-            </div>
-        <div className="buttonSection">
-            <button className="button" onClick={() => removeItemHandler(props.item)}>Remove</button>
-        </div>
-    </div>)
+    return (<div className="cartItem fadeItem">
+                <div className="itemImage">
+                    <img alt="itemImage" src={props.item.thumbnailUrl} />
+                </div>
+                <div className="itemDesc">
+                    {props.item.title.substring(0, 10)}
+                </div>
+                <div className="buttonSection">
+                    <button className="button" onClick={() => removeItemHandler(props.item)}>Remove</button>
+                </div>
+            </div>)
 }
 
 
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        removeItem : (selectedProduct) => dispatch(removeItem(selectedProduct))
+        removeItem: (selectedProduct) => dispatch(removeItem(selectedProduct))
     }
 }
 
